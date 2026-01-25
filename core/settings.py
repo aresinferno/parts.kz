228aa6ca85e76a344f843b605998104dd87a7577
ares <alymbekovimran0@example.com>
2026-01-25 21:33:39 +0600

    обновление базы данных

--- core/settings.py
+++ core/settings.py
@@ -77,6 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -87,8 +93,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
